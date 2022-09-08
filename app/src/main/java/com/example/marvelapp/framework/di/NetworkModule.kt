59dbbca1c09217b50369a1d303b9e2ead4bb192a
Davidson Silva <davidson.f.silva@gmail.com>
2022-09-07 22:11:42 -0300

    Criando o authorization interceptor

--- app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
@@ -5,17 +5,19 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import me.davidsonsilva.core.data.network.interceptor.AuthorizationInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 @Module
 @InstallIn(SingletonComponent::class) //Qual escopo estará disponível
 object NetworkModule {
 
-    private const val TIME_OUT = 15L
+    private const val TIMEOUT_SECONDS = 15L
 
     @Provides
     fun provideLoggingInterceptor():HttpLoggingInterceptor{
@@ -31,13 +33,24 @@ object NetworkModule {
     }
 
     @Provides
+    fun provideAuthorizationInterceptor():AuthorizationInterceptor{
+        return AuthorizationInterceptor(
+            publicKey = BuildConfig.PUBLIC_KEY,
+            privateKey = BuildConfig.PRIVATE_KEY,
+            calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        )
+    }
+
+    @Provides
     fun provideOkHttpClient(
-        loggingInterceptor: HttpLoggingInterceptor
+        loggingInterceptor: HttpLoggingInterceptor,
+        authorizationInterceptor: AuthorizationInterceptor
     ):OkHttpClient{
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
-            .readTimeout(TIME_OUT,TimeUnit.SECONDS)
-            .connectTimeout(TIME_OUT,TimeUnit.SECONDS)
+            .addInterceptor(authorizationInterceptor)
+            .readTimeout(TIMEOUT_SECONDS,TimeUnit.SECONDS)
+            .connectTimeout(TIMEOUT_SECONDS,TimeUnit.SECONDS)
             .build()
     }
 
