b25516a5ca4b5861d892f65b1d9a5007c0d47440
Davidson Silva <davidson.f.silva@gmail.com>
2022-09-24 10:13:50 -0300

    Fixing BaseTest class

--- app/src/test/java/com/example/marvelapp/framework/paging/CharactersPagingSourceTest.kt
+++ app/src/test/java/com/example/marvelapp/framework/paging/CharactersPagingSourceTest.kt
@@ -14,8 +14,11 @@ import me.davidsonsilva.core.domain.model.Character
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class CharactersPagingSourceTest: BaseTest() {
 
     @Mock
