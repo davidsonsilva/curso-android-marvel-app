1ed6ad49bde14db0d67b749ba125d3f267b662ef
Davidson Silva <davidson.f.silva@gmail.com>
2022-10-02 07:46:14 -0300

    Refatorando e implementando um segundo end-point na Api da MArvel

--- app/src/test/java/com/example/marvelapp/factory/response/DataWrapperResponseFactory.kt
+++ app/src/test/java/com/example/marvelapp/factory/response/DataWrapperResponseFactory.kt
@@ -4,17 +4,19 @@ import com.example.marvelapp.framework.network.response.CharacterResponse
 import com.example.marvelapp.framework.network.response.DataContainerResponse
 import com.example.marvelapp.framework.network.response.DataWrapperResponse
 import com.example.marvelapp.framework.network.response.ThumbnailResponse
+import me.davidsonsilva.core.domain.model.Character
+import me.davidsonsilva.core.domain.model.CharacterPaging
 
 class DataWrapperResponseFactory {
 
-    fun create() = DataWrapperResponse(
+    fun create() = DataWrapperResponse<CharacterResponse>(
         copyright = "",
-        data = DataContainerResponse(
+        data = DataContainerResponse<CharacterResponse>(
             offset = 0,
             total = 2,
             results = listOf(
                 CharacterResponse(
-                    id = "1011334",
+                    id = 1011334,
                     name = "3-D Man",
                     thumbnail = ThumbnailResponse(
                         path = "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784",
@@ -23,7 +25,7 @@ class DataWrapperResponseFactory {
 
                 ),
                 CharacterResponse(
-                    id = "1017100",
+                    id = 1017100,
                     name = "A-Bomb (HAS)",
                     thumbnail = ThumbnailResponse(
                         path = "http://i.annihil.us/u/prod/marvel/i/mg/3/20/523215de5b16",
